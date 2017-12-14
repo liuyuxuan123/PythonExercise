8ac16a94c659e3c4b22451e4a0ea6af248063984
liuyuxuan123 <1010134430@qq.com>
2017-12-14 21:57:03 +0800

    updarte

--- ch6/exercise.py
+++ ch6/exercise.py
@@ -38,32 +38,42 @@ print(averageScore(scoreList))
 # 6-6
 
 def nstrip(aStr):
-	# for i in range(len(aStr)):
-	# 	print(aStr[i])
-	# 	if not aStr[i].isspace():
-	# 		a = i
-	# 		break
-	# for j in range(len(aStr) - 1,1,-1):
-	# 	print(aStr[j])
-	# 	if not aStr[j].isspace():
-	# 		b = j
-	# 		break
-
-	while aStr[:1]==' ':
-	    aStr=aStr[1:]
-	while aStr[-1:]==' ':
-   		aStr=aStr[:-1]
-
+	while aStr[:1] == ' ':
+	    aStr = aStr[1:]
+	while aStr[-1:] == ' ':
+   		aStr = aStr[:-1]
 	return aStr
 
 aStrList = ['  asklas',' wqowio  ',' ajskajsk  ','','   ']
-
-print(len(''[:1]),'Test')
 for i in range(len(aStrList)):
 	s = nstrip(aStrList[i])
 	print(s,len(s))
 	#nstrip(aStrList[i])
 
+# 6-8
+def translate(num):
+	#word = ['thousand','hundred']
+
+	num1 = 'One Two Three Four Five Six Seven Eight Nine Ten' \
+	'Eleven Twelve Thirteen Fourteen Fifteen Sixteen Seventeen Eighteen Nineteen'.split()
+	num2 = 'Twelve Thirty Forty Fifty Sixty Seventy Eighty Ninety'.split()
+
+	def words(n):
+		if n < 20:
+			return num1[n - 1:n]
+		elif n < 100:
+			return [num2[n // 10 - 2]] + words(n % 10)
+		elif n < 1000:
+			return [num1[n // 100 - 1]] + ['Hundred'] + words(n % 100)
+		else:
+			for p, w in enumerate(('Thousand', 'Million', 'Billion'), 1):
+				if n < 1000 ** (p + 1):
+					return words(n // 1000 ** p) + [w] + words(n % 1000 ** p)
+
+	print(words(num))
+	return ' '.join(words(num)) or 'Zero'
+
+print(translate(9999))
 
 
 
